afd74fc7a83df597d242fa79f43232c7c583051c
mdmoinulhossain <mdmoinulhossain100@gmail.com>
2022-04-07 16:09:07 +0600

    index page changes

---
+++ adminpdf.php
@@ -0,0 +1,52 @@
+<?php include('./header.php'); ?>
+
+<style>
+.singlePdf {
+    border: 1px solid #000;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 10px;
+}
+.pdf{
+    width: 100% !important
+}
+.mybtn{
+    border: 1px dotted #000000;
+    background-color: #F0F8FF;
+}
+.mybtn:hover{
+    background-color: #F0FFFF;
+}
+</style>
+
+<tbody>
+     
+
+
+<section class="container my-5">
+    <div class="row">
+        <h3>Your PDF's:</h3>
+        <?php
+        include 'config.php';
+
+        $display = "SELECT * FROM `pdflist`";
+
+        $displayQuery = mysqli_query($connect_db, $display);
+
+        while($record = mysqli_fetch_array($displayQuery)){
+
+
+        ?>
+            <div class="col-6 col-md-4 singlePdf mx-2">
+                <embed type="application/pdf" src="pdf/<?php echo $record["pdf"] ?>" class="pdf">
+                <a href="pdf/<?php echo $record["pdf"] ?>" target="_blank">
+                    <button class="btn mybtn mt-3" value="<?php echo $record["pdf"] ?>">Open PDF</button>
+                </a>
+            </div>
+        
+        <?php } ?>
+    </div>
+</section>
+
+<?php include("./footer.php"); ?>
--- index.php
+++ index.php
@@ -1,57 +1,40 @@
-<?php include('./header.php'); ?>
 
 <style>
-.singlePdf {
-    border: 1px solid #000;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 10px;
-}
-.pdf{
-    width: 100% !important
-}
-.mybtn{
-    border: 1px dotted #000000;
-    background-color: #F0F8FF;
-}
-.mybtn:hover{
-    background-color: #F0FFFF;
-}
+    body{
+        padding: 0 !important;
+        margin: 0 !important;
+        box-sizing: border-box !important;
+    }
+    .home{
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        width: 100%;
+        height: 100vh;       
+        background-image: url("./img/homeBanner.webp");
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        position: relative;
+    }
+    .info{
+        text-align: center;
+        position: absolute;
+        top: 35%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        color: white;
+        font-size: 20px;
+    }
+    .info h1{
+        letter-spacing: 3px;
+    }
 </style>
 
-<tbody>
-     
-
-
-<section class="container my-5">
-    <div class="row">
-        <h3>Your PDF's:</h3>
-        <?php
-        include 'config.php';
-
-        $display = "SELECT * FROM `pdflist`";
-
-        $displayQuery = mysqli_query($connect_db, $display);
-
-        if($record = mysqli_fetch_array($displayQuery)){
-
-
-        ?>
-            <div class="col-6 col-md-4 singlePdf mx-2">
-                <embed type="application/pdf" src="pdf/<?php echo $record["pdf"] ?>" class="pdf">
-                <a href="pdf/<?php echo $record["pdf"] ?>" target="_blank">
-                    <button class="btn mybtn mt-3" value="<?php echo $record["pdf"] ?>">Open PDF</button>
-                </a>
-            </div>
-        
-        <?php } else {
-            echo "<h4>No PDF Found</h4>";
-        } ?>   
+<section class="home">
+    <div class="info">
+        <h1>Need any book to read?</h1>
+        <p>Please call: +880 1843-331261</p>
+        <p>Mail us: booklink24@gmail.com</p>
     </div>
 </section>
-
-<!-- Bootstrap js -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-</body>
-</html>
